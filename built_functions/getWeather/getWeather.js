77fa9d9974863efbe30a0725f0ec01f712fdb062
Joseph Flores <yosept.flores@gmail.com>
2020-04-20 09:26:08 -0500

    Trying a new functions folder structure

--- built_functions/getWeather/getWeather.js
+++ built_functions/getWeather/getWeather.js
@@ -1,19 +1,10 @@
 /* eslint-disable */
-// for a full working demo of Netlify Identity + Functions, see https://netlify-gotrue-in-react.netlify.com/
-
 const fetch = require('node-fetch')
 exports.handler = async function(event, context) {
-  if (!context.clientContext && !context.clientContext.identity) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        msg: 'No identity instance detected. Did you enable it?'
-      }) // Could be a custom message or object i.e. JSON.stringify(err)
-    }
-  }
-  const { identity, user } = context.clientContext
   try {
-    const response = await fetch('https://api.chucknorris.io/jokes/random')
+    const response = await fetch('https://icanhazdadjoke.com', {
+      headers: { Accept: 'application/json' }
+    })
     if (!response.ok) {
       // NOT res.status >= 200 && res.status < 300
       return { statusCode: response.status, body: response.statusText }
@@ -22,7 +13,7 @@ exports.handler = async function(event, context) {
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ identity, user, msg: data.value })
+      body: JSON.stringify({ msg: data.joke })
     }
   } catch (err) {
     console.log(err) // output to netlify function log
